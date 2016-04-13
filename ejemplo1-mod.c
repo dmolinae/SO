f0b426043a29431a633a405f4868bf28525c1cad
Daniel <dmolinae@outlook.com>
2016-04-13 15:35:49 -0300

    added ejemplo1-mod

--- ejemplo1-mod.c
+++ ejemplo1-mod.c
@@ -2,14 +2,19 @@
 #include <stdio.h>
 /*programa que ejecuta el mandato ls -1*/
 main() {
-	pid_t pid;
+	pid_t pid[2];
 	int status;
-	pid = fork();
-	if(pid == 0){ /* proceso hijo*/
+	int i;
+	for(i = 0;i < 2;i++){
+		pid[i] = fork();
+		if(pid[i] == 0) break; /* Si es hijo se sale del for */
+	}
+	
+	if(pid[i] == 0){ /* proceso hijo*/
 		execlp("ls","ls","-1",NULL); /* Modifica el texto del proceso */
 		exit(-1);
 	}
 	else /* proceso padre */
-		while(pid != wait(&status));
+		while(pid[i] != wait(&status));
 	exit(0);
 }
